8c416c981e9b599a711754e6817647cc909b259f
Justin <schiller.justin@gmail.com>
2019-09-09 11:19:23 -0500

    Setting up API connection with food2fork.com

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
-// Global app controller
+// API Key: 72dbb3a144a6ec02a44efae7f45a906f
+// Search API: https://www.food2fork.com/api/search
+// Recipe API: https://www.food2fork.com/api/get
+// Proxy: 
 
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Another test! Variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+   const key = '72dbb3a144a6ec02a44efae7f45a906f';
+   const proxy = 'https://cors-anywhere.herokuapp.com/'
+   const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
+   const recipes = res.data.recipes;
+   console.log(recipes);
+}
+
+getResults('pizza');
+
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log('Imported module Yay!');
-export default 23;
